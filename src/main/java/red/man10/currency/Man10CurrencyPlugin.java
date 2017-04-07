ba6cfa8deab0d2e3e54cb0d723d74dad02bba03b
Takashi Ohtsuka <takatronix@gmail.com>
2017-04-08 08:03:52 +0900

    Initial commit

---
+++ src/main/java/red/man10/currency/Man10CurrencyPlugin.java
@@ -0,0 +1,17 @@
+package red.man10.currency;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Man10CurrencyPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
